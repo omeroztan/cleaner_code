bfd7616aa9dcb5fcafcd6aa2186a41a4d8cb9456
omeroztan <omeroztan@users.noreply.github.com>
2020-11-25 13:52:21 +0100

    colab path addition

--- getting_values.py
+++ getting_values.py
@@ -10,29 +10,38 @@ url_test = '/Users/mac/Desktop/Datasets/M4_Dataset/M4_test_set'  # General posit
 class PathManager:
     """sender = DataSender('daily', 3)\n
     test_data_path, train_data_path = sender.send()"""
-    def __init__(self, data_name, index):
+    def __init__(self, data_name, index, colab=0):
         self.name = str(data_name)
         self.index = index
+        self.colab = colab
 
     def send(self):
-        if self.name == 'daily':
-            return url_test + f'/Daily_Test/daily_test_{self.index}.csv', \
-                   url_train + f'/Daily_Train/daily_train_{self.index}.csv'
-        if self.name == 'hourly':
-            return url_test + f'/Hourly_Test/hourly_test_{self.index}.csv', \
-                   url_train + f'/Hourly_Train/hourly_train_{self.index}.csv'
-        if self.name == 'monthly':
-            return url_test + f'/Monthly_Test/monthly_test_{self.index}.csv', \
-                   url_train + f'/Monthly_Train/monthly_train_{self.index}.csv'
-        if self.name == 'quarterly':
-            return url_test + f'/Quarterly_Test/quarterly_test_{self.index}.csv', \
-                   url_train + f'/Quarterly_Train/quarterly_train_{self.index}.csv'
-        if self.name == 'weekly':
-            return url_test + f'/Weekly_Test/weekly_test_{self.index}.csv', \
-                   url_train + f'/Weekly_Train/weekly_train_{self.index}.csv'
-        if self.name == 'yearly':
-            return url_test + f'/Yearly_Test/yearly_test_{self.index}.csv', \
-                   url_train + f'/Yearly_Train/yearly_train_{self.index}.csv'
+        if self.colab == 0:
+            if self.name == 'daily':
+                return url_test + f'/Daily_Test/daily_test_{self.index}.csv', \
+                       url_train + f'/Daily_Train/daily_train_{self.index}.csv'
+            if self.name == 'hourly':
+                return url_test + f'/Hourly_Test/hourly_test_{self.index}.csv', \
+                       url_train + f'/Hourly_Train/hourly_train_{self.index}.csv'
+            if self.name == 'monthly':
+                return url_test + f'/Monthly_Test/monthly_test_{self.index}.csv', \
+                       url_train + f'/Monthly_Train/monthly_train_{self.index}.csv'
+            if self.name == 'quarterly':
+                return url_test + f'/Quarterly_Test/quarterly_test_{self.index}.csv', \
+                       url_train + f'/Quarterly_Train/quarterly_train_{self.index}.csv'
+            if self.name == 'weekly':
+                return url_test + f'/Weekly_Test/weekly_test_{self.index}.csv', \
+                       url_train + f'/Weekly_Train/weekly_train_{self.index}.csv'
+            if self.name == 'yearly':
+                return url_test + f'/Yearly_Test/yearly_test_{self.index}.csv', \
+                       url_train + f'/Yearly_Train/yearly_train_{self.index}.csv'
+        elif self.colab == 1:
+            if self.name == 'daily':
+                return url_test + f'/daily/daily_test_{self.index}.csv', \
+                       url_train + f'/daily/daily_train_{self.index}.csv'
+            if self.name == 'hourly':
+                return url_test + f'/hourly/hourly_test_{self.index}.csv', \
+                       url_train + f'/hourly/hourly_train_{self.index}.csv'
 
 
 class PathToValues:
